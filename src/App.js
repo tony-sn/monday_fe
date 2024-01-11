fcce7730f31dbf55399aafa4e17aa500fcdd86f1
Tony Nguyen <57897153+tony-sn@users.noreply.github.com>
2024-01-12 00:52:34 +1100

    refactor: add alt for img

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import SignUp from './pages/signup/Signup';
+import SignUpEmail from './pages/signupEmail/SignUpEmail';
 
 function App() {
   return (
     <>
       <Routes>
         <Route path='/signup' element={<SignUp />} />
+        <Route path='/signup-email' element={<SignUpEmail />} />
       </Routes>
     </>
   );
--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -1,129 +1,129 @@
-import React from 'react';
+import React from "react";
 
 const SignUp = () => {
   return (
-    <div id='soft-signup-form-container' className='font-monday h-full w-full'>
-      <div className='signup-loader' />
-      <div id='soft-signup-form' className='h-full w-full'>
+    <div id="soft-signup-form-container" className="h-full w-full font-monday">
+      <div className="signup-loader" />
+      <div id="soft-signup-form" className="h-full w-full">
         <div
-          id='soft-signup-component'
-          className='h-full w-full flex justify-center'
+          id="soft-signup-component"
+          className="flex h-full w-full justify-center"
         >
-          <div id='styles-module-wrapper' className='visible relative'>
+          <div id="styles-module-wrapper" className="visible relative">
             <div
-              id='mf-platform-signup'
-              className='w-full h-full flex justify-center items-center'
+              id="mf-platform-signup"
+              className="flex h-full w-full items-center justify-center"
             >
               <div>
                 <div
-                  id='OnCH8'
-                  className='h-screen w-screen grid grid-cols-monday'
+                  id="OnCH8"
+                  className="grid h-screen w-screen grid-cols-monday"
                 >
                   <div
-                    id='monday-left-screen'
-                    className='flex items-center bg-white flex-col h-full'
+                    id="monday-left-screen"
+                    className="flex h-full flex-col items-center bg-white"
                   >
                     <div
-                      id='monday-left-items'
-                      className='flex items-center justify-center flex-[1] flex-col'
+                      id="monday-left-items"
+                      className="flex flex-[1] flex-col items-center justify-center"
                     >
-                      <div id='monday-welcome-wrapper'>
-                        <div className='flex-wrap flex items-center justify-center'>
-                          <h1 className='text-center border-transparent border text-primary-text-color mt-0 mb-2 font-poppins -tracking-[0.5px] px-1 py-0 font-medium antialiased text-[32px]/[40px]'>
+                      <div id="monday-welcome-wrapper">
+                        <div className="flex flex-wrap items-center justify-center">
+                          <h1 className="mb-2 mt-0 border border-transparent px-1 py-0 text-center font-poppins text-[32px]/[40px] font-medium -tracking-[0.5px] text-primary-text-color antialiased">
                             Welcome to monday.com
                           </h1>
                         </div>
-                        <h2 className='text-[16px]/[30px] font-poppins border border-transparent text-center mt-0 mb-12 antialiased px-1 py-0 -tracking-[0.1px] font-medium'>
+                        <h2 className="mb-12 mt-0 border border-transparent px-1 py-0 text-center font-poppins text-[16px]/[30px] font-medium -tracking-[0.1px] antialiased">
                           Get started - it&apos;s free. No credit card needed.
                         </h2>
                       </div>
                       <div
-                        id='monday-left-buttons-wrapper'
-                        className='w-[400px]'
+                        id="monday-left-buttons-wrapper"
+                        className="w-[400px]"
                       >
                         <button
-                          type='button'
-                          className='leading-[22px] font-monday px-[var(--spacing-medium)] py-[var(--spacing-small)] whitespace-nowrap rounded-[4px] inline-flex cursor-pointer items-center justify-center select-none min-w-[auto] h-10 w-full border border-ui-border-color text-primary-text-color transition-[min-width] hover:bg-[var(--primary-background-hover-color)]
+                          type="button"
+                          className="inline-flex h-10 w-full min-w-[auto] cursor-pointer select-none items-center justify-center whitespace-nowrap rounded-[4px] border border-ui-border-color px-[var(--spacing-medium)] py-[var(--spacing-small)] font-monday leading-[22px] text-primary-text-color transition-[min-width] hover:bg-[var(--primary-background-hover-color)]
 
-                          '
+                          "
                         >
                           <img
-                            className='w-4 mr-[var(--spacing-small)]'
-                            src='https://dapulse-res.cloudinary.com/image/upload/remote_logos/995426/google-icon.svg'
+                            alt="Continue with Google"
+                            className="mr-[var(--spacing-small)] w-4"
+                            src="https://dapulse-res.cloudinary.com/image/upload/remote_logos/995426/google-icon.svg"
                           />
                           <span>Continue with Google</span>
                         </button>
-                        <div className='grid gap-[var(--spacing-small)] grid-cols-even my-[var(--spacing-medium)] w-full'>
-                          <div className='self-end border-t border-[var(--layout-border-color)] h-1/2' />
+                        <div className="my-[var(--spacing-medium)] grid w-full grid-cols-even gap-[var(--spacing-small)]">
+                          <div className="h-1/2 self-end border-t border-[var(--layout-border-color)]" />
                           Or
-                          <div className='self-end border-t border-[var(--layout-border-color)] h-1/2' />
+                          <div className="h-1/2 self-end border-t border-[var(--layout-border-color)]" />
                         </div>
-                        {/* TODO: continue inputs and button here*/}
                         <div
-                          id='email-form-container'
-                          className='flex flex-col items-center'
+                          id="email-form-container"
+                          className="flex flex-col items-center"
                         >
-                          <div className='w-full mb-[var(--spacing-medium)]'>
+                          <div className="mb-[var(--spacing-medium)] w-full">
                             <div
                               role
-                              aria-busy='false'
-                              className='relative box-border text-sm w-full'
+                              aria-busy="false"
+                              className="relative box-border w-full text-sm"
                             >
-                              <div id='input-label' className='w-full'>
+                              <div id="input-label" className="w-full">
                                 <div
-                                  id='input-wrapper'
-                                  className='w-full h-10 relative'
+                                  id="input-wrapper"
+                                  className="relative h-10 w-full"
                                 >
                                   <input
-                                    placeholder='name@company.com'
-                                    autoComplete='email'
-                                    type='email'
-                                    data-testid='soft-signup-input-component'
-                                    aria-label='name@company.com'
-                                    aria-invalid='false'
+                                    placeholder="name@company.com"
+                                    autoComplete="email"
+                                    type="email"
+                                    data-testid="soft-signup-input-component"
+                                    aria-label="name@company.com"
+                                    aria-invalid="false"
                                     aria-activedescendant
                                     aria-owns
-                                    className='w-full h-full outline-0 bg-transparent hover:border-primary-text-color focus:border-[var(--primary-color)] border-solid border border-ui-border-color rounded-[4px] text-primary-text-color px-[var(--spacing-medium)] py-[var(--spacing-small)] font-monday text-base text-ellipsis whitespace-nowrap'
+                                    className="h-full w-full text-ellipsis whitespace-nowrap rounded-[4px] border border-solid border-ui-border-color bg-transparent px-[var(--spacing-medium)] py-[var(--spacing-small)] font-monday text-base text-primary-text-color outline-0 hover:border-primary-text-color focus:border-[var(--primary-color)]"
                                   />
                                 </div>
                               </div>
                             </div>
                           </div>
                           <button
-                            type='button'
-                            aria-disabled='false'
-                            aria-busy='false'
-                            className='w-full text-[var(--text-color-on-primary)] text-base px-[var(--spacing-medium)] py-[var(--spacing-small)] transition-[min-width] bg-[var(--primary-color)] border border-ui-border-color rounded-[4px] h-10 inline-flex items-center select-none justify-center min-w-[auto] antialiased'
+                            type="button"
+                            aria-disabled="false"
+                            aria-busy="false"
+                            className="inline-flex h-10 w-full min-w-[auto] select-none items-center justify-center rounded-[4px] border border-ui-border-color bg-[var(--primary-color)] px-[var(--spacing-medium)] py-[var(--spacing-small)] text-base text-[var(--text-color-on-primary)] antialiased transition-[min-width]"
                           >
                             Continue
                           </button>
                         </div>
                         <div
-                          data-testid='terms-container'
-                          className='flex items-center flex-col mt-[var(--spacing-medium)]'
+                          data-testid="terms-container"
+                          className="mt-[var(--spacing-medium)] flex flex-col items-center"
                         >
-                          <div className='flex-wrap justify-center flex items-center text-center'>
-                            <h5 className='m-0 min-w-0 py-0 px-1'>
+                          <div className="flex flex-wrap items-center justify-center text-center">
+                            <h5 className="m-0 min-w-0 px-1 py-0">
                               By proceeding, you agree to the
                             </h5>
                           </div>
-                          <div className='flex-wrap justify-center flex items-center text-center'>
+                          <div className="flex flex-wrap items-center justify-center text-center">
                             <a
-                              data-testid='link'
-                              rel='noreferrer'
-                              href='https://monday.com/l/legal/tos/'
-                              target='_blank'
-                              className='text-[var(--color-link-color)] text-base font-normal hover:underline decoration-[var(--color-link-color)] whitespace-nowrap flex items-center'
+                              data-testid="link"
+                              rel="noreferrer"
+                              href="https://monday.com/l/legal/tos/"
+                              target="_blank"
+                              className="flex items-center whitespace-nowrap text-base font-normal text-[var(--color-link-color)] decoration-[var(--color-link-color)] hover:underline"
                             >
                               <span>Terms of Service</span>
                             </a>
-                            <h5 className='m-0 min-w-0 py-0 px-1'>and</h5>
+                            <h5 className="m-0 min-w-0 px-1 py-0">and</h5>
                             <a
-                              data-testid='link'
-                              href='https://monday.com/l/privacy/privacy-policy.'
-                              rel='noreferrer'
-                              targer='_blank'
-                              className='text-[var(--color-link-color)] text-base font-normal hover:underline decoration-[var(--color-link-color)] whitespace-nowrap flex items-center'
+                              data-testid="link"
+                              href="https://monday.com/l/privacy/privacy-policy."
+                              rel="noreferrer"
+                              targer="_blank"
+                              className="flex items-center whitespace-nowrap text-base font-normal text-[var(--color-link-color)] decoration-[var(--color-link-color)] hover:underline"
                             >
                               <span>Privacy Policy</span>
                             </a>
@@ -132,20 +132,20 @@ const SignUp = () => {
                       </div>
                     </div>
                     <div
-                      id='login'
-                      className='flex items-center flex-col gap-[var(--spacing-small)] mb-16'
+                      id="login"
+                      className="mb-16 flex flex-col items-center gap-[var(--spacing-small)]"
                     >
-                      <div className='flex items-center flex-wrap justify-center'>
-                        <h5 className='m-0 min-w-0 py-0 px-1'>
+                      <div className="flex flex-wrap items-center justify-center">
+                        <h5 className="m-0 min-w-0 px-1 py-0">
                           Already have an account?
                         </h5>
 
                         <a
-                          data-testid='link'
-                          href='/login'
-                          rel='noreferrer'
-                          targer='_blank'
-                          className='text-[var(--color-link-color)] text-base font-normal hover:underline decoration-[var(--color-link-color)] whitespace-nowrap flex items-center'
+                          data-testid="link"
+                          href="/login"
+                          rel="noreferrer"
+                          targer="_blank"
+                          className="flex items-center whitespace-nowrap text-base font-normal text-[var(--color-link-color)] decoration-[var(--color-link-color)] hover:underline"
                         >
                           <span>Log in</span>
                         </a>
@@ -153,12 +153,13 @@ const SignUp = () => {
                     </div>
                   </div>
                   <div
-                    id='monday-right-screen'
-                    className='bg-[rgb(97, 97, 255)] flex justify-center overflow-hidden'
+                    id="monday-right-screen"
+                    className="bg-[rgb(97, 97, 255)] flex justify-center overflow-hidden"
                   >
                     <img
-                      className='h-full w-[unset] max-w-none'
-                      src='https://dapulse-res.cloudinary.com/image/upload/monday_platform/signup/signup-right-side-assets-new-flow/welcome-to-monday.png'
+                      alt="Welcome to monday.com"
+                      className="h-full w-[unset] max-w-none"
+                      src="https://dapulse-res.cloudinary.com/image/upload/monday_platform/signup/signup-right-side-assets-new-flow/welcome-to-monday.png"
                     />
                   </div>
                 </div>
