156f2470ce282fcb0fc1e86579ab4369cd4d0897
Tony Nguyen <57897153+tony-sn@users.noreply.github.com>
2024-01-20 19:34:05 +1100

    refactor: change to constant file

--- src/pages/invitation/Invitation.jsx
+++ src/pages/invitation/Invitation.jsx
@@ -2,62 +2,17 @@ import { Radio } from "antd";
 
 import React, { useState } from "react";
 
-const optionGroup = [
-  {
-    label: "Work",
-    value: "Work",
-    currentRoles: [
-      {
-        label: "Business Owner",
-        value: "Business Owner",
-      },
-      { label: "Team leader", value: "Team leader" },
-      { label: "Team member", value: "Team member" },
-      { label: "Freelancer", value: "Freelancer" },
-      { label: "Director", value: "Director" },
-      { label: "C-Level", value: "C-Level" },
-      { label: "VP", value: "VP" },
-    ],
-  },
-  { label: "Personal", value: "Personal", currentRoles: [{}] },
-  {
-    label: "School",
-    value: "School",
-    currentRoles: [
-      { label: "Undergraduate student", value: "Undergraduate student" },
-      { label: "Graduate student", value: "Graduate student" },
-      { label: "Faculty member", value: "Faculty member" },
-      { label: "Other", value: "Other" },
-    ],
-  },
-  {
-    label: "Nonprofits",
-    value: "Nonprofits",
-    currentRoles: [
-      {
-        label: "Board member",
-        value: "Board member",
-      },
-      { label: "Executive", value: "Executive" },
-      { label: "Employee", value: "Employee" },
-      { label: "Volunteer", value: "Volunteer" },
-      { label: "IT Staff", value: "IT Staff" },
-      { label: "Other", value: "Other" },
-    ],
-  },
-];
+import { optionGroup } from "./constant";
 
 const Invitation = () => {
   const [selected, setSelected] = useState("Work");
   const [selectedRole, setSelectedRole] = useState("");
 
   const onChange = ({ target: { value } }) => {
-    console.log("radio3 checked", value);
     setSelected(value);
     setSelectedRole("");
   };
   const onChangeRole = ({ target: { value } }) => {
-    console.log("radio role checked", value);
     setSelectedRole(value);
   };
   const chosenRoleGroup = optionGroup.find(
---
+++ src/pages/invitation/constant.js
@@ -0,0 +1,44 @@
+export const optionGroup = [
+  {
+    label: "Work",
+    value: "Work",
+    currentRoles: [
+      {
+        label: "Business Owner",
+        value: "Business Owner",
+      },
+      { label: "Team leader", value: "Team leader" },
+      { label: "Team member", value: "Team member" },
+      { label: "Freelancer", value: "Freelancer" },
+      { label: "Director", value: "Director" },
+      { label: "C-Level", value: "C-Level" },
+      { label: "VP", value: "VP" },
+    ],
+  },
+  { label: "Personal", value: "Personal", currentRoles: [{}] },
+  {
+    label: "School",
+    value: "School",
+    currentRoles: [
+      { label: "Undergraduate student", value: "Undergraduate student" },
+      { label: "Graduate student", value: "Graduate student" },
+      { label: "Faculty member", value: "Faculty member" },
+      { label: "Other", value: "Other" },
+    ],
+  },
+  {
+    label: "Nonprofits",
+    value: "Nonprofits",
+    currentRoles: [
+      {
+        label: "Board member",
+        value: "Board member",
+      },
+      { label: "Executive", value: "Executive" },
+      { label: "Employee", value: "Employee" },
+      { label: "Volunteer", value: "Volunteer" },
+      { label: "IT Staff", value: "IT Staff" },
+      { label: "Other", value: "Other" },
+    ],
+  },
+];
